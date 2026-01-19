aa07900a2b46e368098c5dbd74f5c834548992b2
Husam-AbuZina <husamzinap@gmail.com>
2026-01-19 21:30:54 +0200

    fix deployment

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Neuroroads_Portfolio_Official/', // اسم الريبو على GitHub
-  build: {
-    outDir: 'dist', // عشان GitHub Pages يقرأ من مجلد docs
-  },
+  base: '/',
+  build: { outDir: 'dist' },
 })
+
